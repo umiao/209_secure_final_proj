fix the bug of gradient solving

main.py
@@ -4,6 +4,8 @@ import torch
 from server import client
 from model import Net
 import numpy as np
+import copy
+import torch.nn as nn
 
 os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 random_seed = 1
@@ -16,7 +18,7 @@ param_dict = {'n_epochs' : 1,
               'learning_rate' : 0.001, # 0.01  持续5个batch 然后改为0.001再来5个
               'momentum' : 0.5,
               'log_interval' : 10,
-              'train_set_num' : 20000, # scale of training set possessed by the client
+              'train_set_num' : 6000, # scale of training set possessed by the client
               'class_distribution' : [0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1, 0.1]
               # the distribution of the samples of each class
               }
@@ -27,33 +29,24 @@ Master = Net(params = param_dict, retrieve_history=False)
 #.train_single_batch()
 torch.save(Master.state_dict(), './results/model.pth')
 torch.save(Master.optimizer.state_dict(), './results/optimizer.pth')
-master_model_dict = Master.state_dict()
+master_model_dict = copy.deepcopy(Master.state_dict())
 
-# generate 10 clients
-#distribution_lst = [[0.8, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-#                    [0.0, 0.8, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0],
-#                    [0.0, 0.0, 0.8, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0],
-#                    [0.0, 0.0, 0.0, 0.8, 0.1, 0.1, 0.0, 0.0, 0.0, 0.0],
-#                    [0.0, 0.0, 0.0, 0.0, 0.8, 0.1, 0.1, 0.0, 0.0, 0.0],
-#                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.1, 0.1, 0.0, 0.0],
-#                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.1, 0.1, 0.0],
-#                    [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.1, 0.1],
-#                    [0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8, 0.1],
-#                    [0.1, 0.1, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.8],
-#                    ]
-distribution_lst = np.random.dirichlet(np.ones(10),size=10).tolist()
-#print(distribution_lst)
 client_num = 10
+epoch_size = 1000
+distribution_lst = np.random.dirichlet(np.ones(10),size=client_num).tolist()
+#print(distribution_lst)
 client_lst = []
 #Master = client(params=param_dict, retrieve_history=False)
 
 for c in range(0, client_num):
     param_dict['class_distribution'] = distribution_lst[c]
-    client_tmp = client(retrieve_history=True, params=param_dict)
+    # client_tmp = client(retrieve_history=True, params=param_dict)
+    client_tmp = client(retrieve_history=False, params=param_dict)
     client_lst.append(client_tmp)
 #print(client_lst)
+Master.test()
 
-for epoch in range(0,20):
+for epoch in range(0, epoch_size):
     print("epoch:{}/20".format(epoch))
     # train each client and compute gradient
     grad_lst = []
@@ -67,15 +60,17 @@ for epoch in range(0,20):
         for grad in grad_lst:
             sum_tmp += grad[param_name]
         client_grad_mean[param_name] = (sum_tmp / len(grad_lst))
+        nn.utils.clip_grad_norm(client_grad_mean[param_name], max_norm=20, norm_type=2)
     #print(client_grad_mean)
     # update to master's model
     for param_name in master_model_dict:
+        # master_model_dict[param_name] += client_grad_mean[param_name]
         master_model_dict[param_name] += client_grad_mean[param_name]
     #print(master_model_dict)
     # update to client's model
     for client in client_lst:
         client.local_model.load_state_dict(master_model_dict)
 
-Master.load_state_dict(master_model_dict)
-Master.test()
-
+    Master.load_state_dict(master_model_dict)
+    Master.test()
+torch.save(Master.state_dict(), './results/model.pth')

model.py
@@ -4,6 +4,7 @@ import torch
 import torchvision
 import torch.optim as optim
 from partition_dataset import partition
+import copy
 
 class Net(nn.Module):
     n_epochs = 5
@@ -111,14 +112,19 @@ class Net(nn.Module):
         for batch_idx, (data, target) in enumerate(self.train_loader):
             self.optimizer.zero_grad()
             output = self(data)
+            old_named_parameters = {}
+            for name, tensor_v in self.named_parameters():
+                old_named_parameters[name] = copy.deepcopy(tensor_v)
+
             loss = F.nll_loss(output, target)
-            loss.backward(retain_graph=True)
+            # loss.backward(retain_graph=True)
+            loss.backward()
+            nn.utils.clip_grad_norm(self.parameters(), max_norm=20, norm_type=2)
             self.optimizer.step()
-            #computed_gradient = []
             computed_gradient = {}
             for name, tensor_v in self.named_parameters():
                 #computed_gradient.append(tensor_v.grad)
-                computed_gradient[name] = tensor_v.grad
+                computed_gradient[name] = tensor_v - old_named_parameters[name]
             return computed_gradient
 
 